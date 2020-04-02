87f6d4af75c086b276dbdfae55da4b0bf771a3eb
Puru <63047454+PuruJohri@users.noreply.github.com>
2020-04-02 17:01:40 +0530

    Update file1.c

--- file1.c
+++ file1.c
@@ -53,13 +53,10 @@ void display(struct student list[80], int s)
     int i,AvgWaitingTime=0,AvgTurnAroundTime=0;
 	int TotalWatingTime=0,TotalTurnAroundTime=0;
     printf("\n\n\t\t\tOutput according to LRTF\n");
-    printf("\n\t\t\t|===============================================================|");
-    printf("\n\t\t\t|Student id\tFoodTakenTime\tWaitingTime\tTurnAroundTime  |");
-    printf("\n\t\t\t|===============================================================|");    
+    printf("\n\t\t\t|Student id\tFoodTakenTime\tWaitingTime\tTurnAroundTime  |");    
     for (i = 0; i < s; i++)
     {
         printf("\n\t\t\t|%d\t\t%d\t\t%d\t\t%d\t\t|", list[i].StudentId, list[i].FoodTakenTime,list[i].WaitingTime,list[i].TurnAroundTime);
-        printf("\a\n\t\t\t|---------------------------------------------------------------|");
 		TotalWatingTime= TotalWatingTime+list[i].WaitingTime;
 		TotalTurnAroundTime= TotalTurnAroundTime+list[i].TurnAroundTime;
 	} 
@@ -67,53 +64,4 @@ void display(struct student list[80], int s)
 	printf("\n\t\t\tTotal Turn around Time is: = %d\n\n",TotalTurnAroundTime);
 	printf("\n\n\t\t\tAverage Waiting Time is: = %d",TotalWatingTime/s);
 	printf("\n\t\t\tAverage Turn around Time is: = %d\n\n",TotalTurnAroundTime/s);
-}
-void scheduling(struct student list[80], int s)
-{
-    int i, j;
-    struct student temp;
-    
-    for (i = 0; i < s - 1; i++)
-    {
-        for (j = 0; j < (s - 1-i); j++)
-        {
-            if (list[j].FoodTakenTime < list[j + 1].FoodTakenTime)
-            {
-                temp = list[j];
-                list[j] = list[j + 1];
-                list[j + 1] = temp;
-            } 
-            else if(list[j].FoodTakenTime == list[j + 1].FoodTakenTime)
-            {
-            	if(list[j].StudentId > list[j + 1].StudentId)
-            	{
-            	temp = list[j];
-                list[j] = list[j + 1];
-                list[j + 1] = temp;
-                }
-			}
-        }
-    }
-}
-
-
-void waitingTime(struct student list[80], int n)
-{
-	int j,total;
-    list[0].WaitingTime=0;
-    for(j=1;j<n;j++)
-    {
-        list[j].WaitingTime=list[j-1].WaitingTime+list[j-1].FoodTakenTime;
-    }
-}
-
-
-void turnAroundTime(struct student list[80], int n)
-{
-	int j,total;
-    
-    for(j=0;j<n;j++)
-    {
-        list[j].TurnAroundTime=list[j].WaitingTime+list[j].FoodTakenTime;
-    }
 }
